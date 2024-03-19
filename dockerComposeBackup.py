27e251c72dcefb47da43faa02ca2060b47a4df4d
oPisiti <78967454+oPisiti@users.noreply.github.com>
2024-03-19 15:48:00 -0300

    Features addition
    - Command line arguments
    - Optional encryption
    - Optional appending to archive

--- dockerComposeBackup.py
+++ dockerComposeBackup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import argparse
 import getpass
 import os
 from pathlib import Path
@@ -9,36 +10,51 @@ import subprocess
 
 
 def main():
+    # Parsing command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--docker-path", help="Sets the directory in which the docker files are located. Default: ~/docker-Containers")
+    parser.add_argument("--output-dir", help="Sets a custom output directory. Default: ~/")
+    parser.add_argument("--output-name", help="Sets a custom output file name. Default: ~/docker-compose-archive.tar.7z")
+    parser.add_argument("-e", help="Enables encryption", action='store_true')
+    parser.add_argument("-a", help="Appends files to archive if it already exists", action='store_true')
+    args = parser.parse_args()
+
     # Configuration info
-    home_dir = str(Path.home())
+    HOME_DIR = str(Path.home())
     
-    docker_files_dir = home_dir + "/docker-Containers/"     # ADJUST TO YOUR SETUP
-    docker_regex = ".*docker-compose.yml$"
+    if not args.docker_path: DOCKER_FILES_DIR = HOME_DIR + "/docker-Containers/"     # ADJUST TO YOUR SETUP
+    else:                    DOCKER_FILES_DIR = args.docker_path
+
+    DOCKER_REGEX = ".*docker-compose.yml$"
     
-    tmp_dir_name = "docker-compose-backup.tmp/"
-    tmp_dir_path = f"{home_dir}/{tmp_dir_name}"
+    TMP_DIR_NAME = "docker-compose-backup.tmp/"
+    TMP_DIR_PATH = f"{HOME_DIR}/{TMP_DIR_NAME}"
     
-    output_dir = home_dir + "/"
-    output_file_name = "docker-compose-archive.tar.7z"
+    if not args.output_dir:  OUTPUT_DIR = HOME_DIR + "/"
+    else:                    OUTPUT_DIR = args.output_dir
+
+    if not args.output_name: OUTPUT_FILE_NAME = "docker-compose-archive.tar.7z"
+    else:                    OUTPUT_FILE_NAME = args.output_name + ".tar.7z"
     
-    encrypt = True
+    ENCRYPT = args.e
+    APPEND  = args.a
 
     # Creating a tmp file
     try:
-        os.mkdir(tmp_dir_path)
+        os.mkdir(TMP_DIR_PATH)
     except FileExistsError as e:
-        confirm = input(f"File {tmp_dir_path} already exists. Removing and recreate? (y/N) ")
+        confirm = input(f"File {TMP_DIR_PATH} already exists. Removing and recreate? (y/N) ")
         if confirm != "y": return
         
-        subprocess.run(f"sudo rm -r {tmp_dir_path}", shell=True, text=True)
-        os.mkdir(tmp_dir_path)
+        subprocess.run(f"sudo rm -r {TMP_DIR_PATH}", shell=True, text=True)
+        os.mkdir(TMP_DIR_PATH)
         
     # Getting all docker-compose.yml paths
-    compose_paths = subprocess.run(f"sudo find {docker_files_dir} -maxdepth 2 -type f -regex '{docker_regex}'", shell=True, text=True, stdout=subprocess.PIPE).stdout
+    compose_paths = subprocess.run(f"sudo find {DOCKER_FILES_DIR} -maxdepth 2 -type f -regex '{DOCKER_REGEX}'", shell=True, text=True, stdout=subprocess.PIPE).stdout
     compose_paths = compose_paths.split("\n")
     
     # The expected number of subdirectories of a path for a top level docker-compose.yml file
-    expected_size = len(docker_files_dir.split("/")) + 1
+    expected_size = len(DOCKER_FILES_DIR.split("/")) + 1
     
     # Cleaning the bigger paths - They include sub-sub dirs docker-compose.yml files.
     # Theses are probably for dependencies. They were NOT made by me
@@ -50,9 +66,9 @@ def main():
             compose_paths_dict[p] = dirs
     
     
-    # Creating directories in tmp_dir_name and moving docker-compose.yml files
+    # Creating directories in TMP_DIR_NAME and moving docker-compose.yml files
     for path, parts in compose_paths_dict.items():
-        dir_to_create = f"{tmp_dir_path}{parts[-2]}"
+        dir_to_create = f"{TMP_DIR_PATH}{parts[-2]}"
         
         # Creating dir
         try:
@@ -64,9 +80,13 @@ def main():
         # Moving docker-compose.yml files
         subprocess.run(f"sudo cp {path} {dir_to_create}", shell=True, text=True)
         
+    # Deleting pre-existing archive is -a not set
+    if not APPEND and os.path.exists(OUTPUT_DIR + OUTPUT_FILE_NAME):
+        print(f"File {OUTPUT_DIR + OUTPUT_FILE_NAME} exists. Deleting")
+        subprocess.run(f"sudo rm -r {OUTPUT_DIR + OUTPUT_FILE_NAME}", shell=True, text=True)
 
     # Prompting for password
-    if encrypt:
+    if ENCRYPT:
         while True:
             try:
                 password = getpass.getpass(prompt="Encryption password:")
@@ -80,11 +100,12 @@ def main():
 
     
     # Encrypting with password
-    if encrypt: subprocess.run(f"cd {home_dir} && tar -cf - {tmp_dir_name} | 7za a -p'{password}' -si {output_dir}{output_file_name}", shell=True, text=True)    
-    else:       subprocess.run(f"cd {home_dir} && tar -cf - {tmp_dir_name} | 7za a -si {output_dir}{output_file_name}", shell=True, text=True)    
+    if ENCRYPT: subprocess.run(f"cd {HOME_DIR} && tar -cf - {TMP_DIR_NAME} | 7za a -p'{password}' -si {OUTPUT_DIR}{OUTPUT_FILE_NAME}", shell=True, text=True)    
+    else:       subprocess.run(f"cd {HOME_DIR} && tar -cf - {TMP_DIR_NAME} | 7za a -si {OUTPUT_DIR}{OUTPUT_FILE_NAME}", shell=True, text=True)    
     
     # Cleaning up tmp dir
-    subprocess.run(f"sudo rm -r {tmp_dir_path}", shell=True, text=True)
+    subprocess.run(f"sudo rm -r {TMP_DIR_PATH}", shell=True, text=True)
+
 
 
 if __name__ == "__main__":
